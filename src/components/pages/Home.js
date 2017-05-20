c2c7b97b3576716401853bfe0ba6b38d729d6a54
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-20 18:53:19 +0200

    Fix FrontPage appbar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import SubredditContainer from '../../containers/SubredditContainer'
+import AppBar from '../widgets/AppBar'
 
-const Home = () => <SubredditContainer />
+const Home = () => (
+  <div>
+    <AppBar />
+    <SubredditContainer />
+  </div>
+)
 
 export default Home
