b5e3372fd69eae4abf644c21f0dd7446a342d113
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-28 20:40:15 +0200

    Add tests for subreddits reducer

---
+++ src/reducers/subreddits.spec.js
@@ -0,0 +1,123 @@
+import { byId } from './subreddits'
+
+describe('byId', () => {
+  it('Should return the initial state', () => {
+    expect(
+      byId(undefined, {})
+    )
+    .toEqual({})
+  })
+
+  it('Should handle RECEIVE_SUBREDDIT (insert)', () => {
+    expect(
+      byId(
+        // State
+        {
+          sub1: {
+            id: 'sub1',
+            display_name: 'webdev'  
+          },
+          sub2: {
+            id: 'sub2',
+            display_name: 'kotlin'  
+          }
+        },
+        // Action
+        {
+          type: 'RECEIVE_SUBREDDIT',
+          subreddit: {
+            id: 'sub3',
+            display_name: 'reduxjs'
+          }
+        }
+      )
+    )
+    .toEqual({
+        sub1: {
+          id: 'sub1',
+          display_name: 'webdev'  
+        },
+        sub2: {
+          id: 'sub2',
+          display_name: 'kotlin'  
+        },
+        sub3: {
+          id: 'sub3',
+          display_name: 'reduxjs'
+        }
+    })
+  })
+
+  it('Should handle RECEIVE_SUBREDDIT (update)', () => {
+    expect(
+      byId(
+        // State
+        {
+          sub1: {
+            id: 'sub1',
+            display_name: 'webdev'  
+          },
+          sub2: {
+            id: 'sub2',
+            display_name: 'kotlin'  
+          }
+        },
+        // Action
+        {
+          type: 'RECEIVE_SUBREDDIT',
+          subreddit: {
+            id: 'sub2',
+            display_name: 'reduxjs'
+          }
+        }
+      )
+    )
+    .toEqual({
+        sub1: {
+          id: 'sub1',
+          display_name: 'webdev'  
+        },
+        sub2: {
+          id: 'sub2',
+          display_name: 'reduxjs'
+        }
+    })
+  })
+
+  const same = ['RECEIVE_SUBSCRIPTIONS', 'RECEIVE_SUBREDDITS']
+
+  same.forEach(type => it(`Should handle ${type}`, () => {
+    expect(
+      byId(
+        // State
+        {
+          sub1: {
+            id: 'sub1',
+            display_name: 'kotlin'
+          }
+        },
+        // Action
+        {
+          type,
+          subreddits: [{
+            id: 'sub2',
+            display_name: 'reactjs'
+          }, {
+            id: 'sub1',
+            display_name: 'webdev'
+          }]
+        }
+      )
+    )
+    .toEqual({
+      sub1: {
+        id: 'sub1',
+        display_name: 'webdev'
+      },
+      sub2: {
+        id: 'sub2',
+        display_name: 'reactjs'
+      }
+    })
+  }))
+})
