df2801a66886510fb98833c7ed21b420d65f2cae
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-28 21:45:46 +0200

    Add prefs reducer

---
+++ src/reducers/prefs.js
@@ -0,0 +1,36 @@
+import { combineReducers } from 'redux'
+import * as actions from '../actions/prefs'
+
+export const prefs = (state = null, action) => {
+  switch (action.type) {
+    case actions.RECEIVE_PREFS:
+      return action.prefs
+
+    default:
+      return state
+  }
+}
+
+export const api = (state = { isLoading: false }, action) => {
+  switch (action.type) {
+    case actions.REQUEST_PREFS:
+      return {
+        ...state,
+        isLoading: true
+      }
+    
+    case actions.RECEIVE_PREFS:
+      return {
+        ...state,
+        isLoading: false
+      }
+    
+    default:
+      return state
+  }
+}
+
+export default combineReducers({
+  api,
+  prefs
+})
---
+++ src/reducers/prefs.spec.js
@@ -0,0 +1,58 @@
+import { prefs, api } from './prefs'
+
+describe('prefs', () => {
+  it('Should return the initial state', () => {
+    expect(
+      prefs(undefined, {})
+    )
+    .toEqual(null)
+  })
+
+  it('Should handle RECEIVE_PREFS', () => {
+    expect(
+      prefs(
+        // State
+        null,
+        // Action
+        {
+          type: 'RECEIVE_PREFS',
+          prefs: { foo: 'bar' }
+        }
+      )
+    )
+    .toEqual({ foo: 'bar' })
+  })
+})
+
+describe('api', () => {
+  it('Should return the initial state', () => {
+    expect(
+      api(undefined, {})
+    )
+    .toEqual({ isLoading: false })
+  })
+
+  it('Should handle REQUEST_PREFS', () => {
+    expect(
+      api(
+        // State
+        { isLoading: false },
+        // Action
+        { type: 'REQUEST_PREFS' }
+      )
+    )
+    .toEqual({ isLoading: true })
+  })
+
+  it('Should handle RECEIVE_PREFS', () => {
+    expect(
+      api(
+        // State
+        { isLoading: true },
+        // Action
+        { type: 'RECEIVE_PREFS' }
+      )
+    )
+    .toEqual({ isLoading: false })
+  })
+})
