ebe9264b26609f1ddb802a8b17307af64d4cd70d
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-29 22:31:42 +0200

    Add subscription actions

--- src/actions/subreddit.js
+++ src/actions/subreddit.js
@@ -2,6 +2,9 @@ export const REQUEST_SUBREDDIT = 'REQUEST_SUBREDDIT'
 export const RECEIVE_SUBREDDIT = 'RECEIVE_SUBREDDIT'
 export const RECEIVE_SUBREDDITS = 'RECEIVE_SUBREDDITS'
 export const RECEIVE_SUBSCRIPTIONS = 'RECEIVE_SUBSCRIPTIONS'
+export const TOGGLE_SUBSCRIPTION = 'TOGGLE_SUBSCRIPTION'
+export const ADD_SUBSCRIPTION = 'ADD_SUBSCRIPTION'
+export const REMOVE_SUBSCRIPTION = 'REMOVE_SUBSCRIPTION'
 
 export const requestSubreddit = name => ({
   type: REQUEST_SUBREDDIT,
@@ -21,4 +24,19 @@ export const receiveSubreddits = subreddits => ({
 export const receiveSubscriptions = subreddits => ({
   type: RECEIVE_SUBSCRIPTIONS,
   subreddits
+})
+
+export const toggleSubscription = id => ({
+  type: TOGGLE_SUBSCRIPTION,
+  id
+})
+
+export const addSubscription = id => ({
+  type: ADD_SUBSCRIPTION,
+  id
+})
+
+export const removeSubscription = id => ({
+  type: REMOVE_SUBSCRIPTION,
+  id
 })
