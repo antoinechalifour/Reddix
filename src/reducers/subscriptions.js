17c65e3086a962c99e35f309976ad06cab610c6c
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-29 22:31:25 +0200

    Add subscription actions to reducer

--- src/reducers/subscriptions.js
+++ src/reducers/subscriptions.js
@@ -5,6 +5,12 @@ const subscriptionsReducer = (state = [], action) => {
     case actions.RECEIVE_SUBSCRIPTIONS:
       return action.subreddits.map(x => x.id)
     
+    case actions.ADD_SUBSCRIPTION:
+      return [...state, action.id]
+
+    case actions.REMOVE_SUBSCRIPTION:
+      return state.filter(id => id !== action.id)
+
     default:
       return state
   }
--- src/reducers/subscriptions.spec.js
+++ src/reducers/subscriptions.spec.js
@@ -25,4 +25,34 @@ describe('subscriptions', () => {
     )
     .toEqual(['sub2', 'sub3'])
   })
+
+  it('Should handle ADD_SUBSCRIPTION', () => {
+    expect(
+      reducer(
+        // State
+        ['sub1'],
+        // Action
+        {
+          type: 'ADD_SUBSCRIPTION',
+          id: 'sub2'
+        }
+      )
+    )
+    .toEqual(['sub1', 'sub2'])
+  })
+
+  it('Should handle REMOVE_SUBSCRIPTION', () => {
+    expect(
+      reducer(
+        // State
+        ['sub1', 'sub2', 'sub2'],
+        // Action
+        {
+          type: 'REMOVE_SUBSCRIPTION',
+          id: 'sub2'
+        }
+      )
+    )
+    .toEqual(['sub1'])
+  })
 })
