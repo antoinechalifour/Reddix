e8c415cc587a9485d6ca35773998d8d903fb715e
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-30 21:34:26 +0200

    Fix authentication

--- src/sagas/auth.js
+++ src/sagas/auth.js
@@ -1,5 +1,6 @@
 import qs from 'querystring'
-import { all, take, put, fork, select } from 'redux-saga/effects'
+import { all, take, put, fork } from 'redux-saga/effects'
+import { push } from 'react-router-redux'
 import Api from '../api/Api'
 import * as AuthActions from '../actions/auth'
 import * as SubredditActions from '../actions/subreddit'
@@ -8,20 +9,6 @@ import * as MeActions from '../actions/me'
 const REFRESH_TOKEN_KEY = 'REDDIX::refresh_token'
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
 const REDIRECT_URL = process.env.REACT_APP_OAUTH_REDIRECT_URL
-
-const watchOauthWindow = wOauth => new Promise((resolve, reject) => {
-  const interval = setInterval(() => {
-    try {
-      const href = wOauth.location.href
-      const query = qs.parse(href)
-      resolve(query.code)
-      clearInterval(interval)
-      wOauth.close()
-    } catch (err) {
-      // Cannot read when on reddit oauth page
-    }
-  }, 2000)
-})
 
 function * setUp (r) {
   // In the set up, we want to fetch:
@@ -85,8 +72,29 @@ function * loginFromLocalStorage () {
   }
 }
 
+function * loginFromAuthCode () {
+  const href = window.location.href
+  const { code } = qs.parse(href)
+
+  if (!code) {
+    return
+  }
+
+  const r = yield Api.fromAuthCode({
+    code,
+    clientId: CLIENT_ID,
+    redirectUri: REDIRECT_URL
+  })
+
+  // localStorage.setItem(REFRESH_TOKEN_KEY, r.refreshToken)
+
+  yield setUp(r)
+  yield put(push('/'))
+}
+
 function * loginFlow () {
   yield loginFromLocalStorage()
+  yield loginFromAuthCode()
 
   while (true) {
     yield take(AuthActions.LOGIN)
@@ -109,25 +117,7 @@ function * loginFlow () {
       redirectUri: REDIRECT_URL
     })
 
-    const wOauth = window.open(authUrl, "Authorize Reddix", "width=800, height=600")
-  
-    const code = yield watchOauthWindow(wOauth)
-
-    if (!code) {
-      yield put(AuthActions.LOGIN_FAILED)
-      return
-    }
-
-    const r = yield Api.fromAuthCode({
-      code,
-      clientId: CLIENT_ID,
-      redirectUri: REDIRECT_URL
-    })
-
-    localStorage.setItem(REFRESH_TOKEN_KEY, r.refreshToken)
-
-    yield setUp(r)
-
+    window.location = authUrl
   }
 }
 
@@ -137,8 +127,8 @@ function * logout () {
     yield take(AuthActions.LOGOUT_REQUEST)
 
     // 1. Revoke the refresh token
-    const r = yield select(state => state.auth)
-    yield r.revokeRefreshToken()
+    // const r = yield select(state => state.r)
+    // yield r.revokeRefreshToken()
 
     // 2. Remove the refresh token from the locale storage
     localStorage.removeItem(REFRESH_TOKEN_KEY)
