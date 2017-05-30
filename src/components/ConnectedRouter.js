bbe1c3f8416da9c904d7d838b6ce4732db191ecd
antoinechalifour <antoine.chalifour@gmail.com>
2017-05-30 21:32:43 +0200

    Add internal implementation of connected router

---
+++ src/components/ConnectedRouter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Router } from 'react-router-dom'
+
+const LOCATION_CHANGE = '@@router/LOCATION_CHANGE'
+
+class ConnectedRouter extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleLocationChange = this.handleLocationChange.bind(this)
+  }
+
+  componentWillMount () {
+    const { history } = this.props
+    this.unsubscribe = history.listen(this.handleLocationChange)
+    this.handleLocationChange(history.location)
+  }
+
+  componentWillUnmount () {
+    this.unsubscribe()
+  }
+
+  handleLocationChange (location) {
+    this.props.store.dispatch({
+      type: LOCATION_CHANGE,
+      payload: location
+    })
+  }
+
+  render () {
+    return <Router {...this.props} />
+  }
+}
+
+export default ConnectedRouter
