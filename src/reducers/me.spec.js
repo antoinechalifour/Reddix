bf4a36ae76f26756b100d7deba03c571ebf9cd41
antoinechalifour <antoine.chalifour@gmail.com>
2017-06-03 10:16:29 +0200

    Add tests for me reducer

---
+++ src/reducers/me.spec.js
@@ -0,0 +1,29 @@
+import reducer from './me'
+
+describe('me', () => {
+  it('Should return the initial state', () => {
+    expect(reducer(undefined, {}))
+    .toEqual(null)
+  })
+
+  const states = [null, { foo: 'bar' }]
+
+
+  states.forEach(state => it(`Should handle RECEIVE_ME`, () => {
+    expect(
+      reducer(
+        state,{
+          type: 'RECEIVE_ME',
+          me: {
+            id: 'some-user-id',
+            icon_img: 'some-icon-url'
+          }
+        }
+      )
+    )
+    .toEqual({
+      id: 'some-user-id',
+      icon_img: 'some-icon-url'
+    })
+  }))
+})
