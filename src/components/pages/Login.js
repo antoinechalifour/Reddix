65ae294d7231cf09942b664fe36c4b28ec095ee4
antoinechalifour <antoine.chalifour@gmail.com>
2017-06-04 19:05:53 +0200

    Refactor LoginPage to use styled-components

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,23 +1,92 @@
 import React from 'react'
+import styled from 'styled-components'
 import InfiniteProgressWrapper from '../widgets/InfiniteProgressWrapper'
+import {
+  PRIMARY_COLOR,
+  BOX_SHADOW_1,
+  BOX_SHADOW_2
+} from '../../util/constants'
+
+const Outer = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+
+  background: ${PRIMARY_COLOR};
+  color: #fff;
+`
+
+const Title = styled.h1`
+  font-size: 6rem;
+  text-transform: uppercase;
+`
+
+const Button = styled.button`
+  border: none;
+  font-size: inherit;
+  color: inherit;
+
+  padding: 16px 32px;
+  border-radius: 4px;
+
+  cursor: pointer;
+  text-transform: uppercase;
+  
+  background: rgba(0, 0, 0, .2);
+  box-shadow: ${BOX_SHADOW_1};
+  transition: box-shadow .2s ease-in;
+
+  &:hover {
+    box-shadow: ${BOX_SHADOW_2}
+  }
+`
+
+const ButtonWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+`
+
+const ProgressWrapper = styled.div`
+  > div {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+  }
+`
+
+const Progress = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, .2);
+`
 
 const Login = ({ actions, isLoading }) => (
-  <div className='login-page'>
-    <div className="login-page__content">
-      <div className="login-page__title">Reddix</div>
+  <Outer>
+    <div>
+      <Title>Reddix</Title>
 
-      <div className='login-page__button'>
+      <ButtonWrapper>
         {isLoading && (
-          <div className='login-page__progress-wrapper'>
+          <ProgressWrapper>
             <InfiniteProgressWrapper>
-              <div className='login-page__progress' />
+              <Progress />
             </InfiniteProgressWrapper>
-          </div>
+          </ProgressWrapper>
         )}
-        <button onClick={() => actions.loginRequest()}>Login with Reddit</button>
-      </div>
+        <Button onClick={() => actions.loginRequest()}>
+          Login with Reddit
+        </Button>
+      </ButtonWrapper>
     </div>
-  </div>
+  </Outer>
 )
 
 export default Login
---
+++ src/util/constants.js
@@ -0,0 +1,4 @@
+export const PRIMARY_COLOR = '#2196f3'
+export const ACCENT_COLOR = '#ff003c'
+export const BOX_SHADOW_1 = '0 1px 3px rgba(0, 0, 0, .13)'
+export const BOX_SHADOW_2 = '0 1px 3px rgba(0, 0, 0, .24)'
