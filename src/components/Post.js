86203badb27026031df7eebb22ff86b135f65e92
antoinechalifour <antoine.chalifour@gmail.com>
2017-06-05 11:06:29 +0200

    Refactor Post to use styled-components

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,74 +1,53 @@
 import React, { Component } from 'react'
 import Markdown from 'react-markdown'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import AppBarContainer from '../containers/AppBarContainer'
 import ThreadHeader from './ThreadHeader'
+import ThreadInformation from './ThreadInformation'
+import ThreadScore from './ThreadScore'
 import CommentsContainer from '../containers/CommentsContainer'
 import FluidIframe from './FluidIframe'
+import {
+  RESPONSIVE_BREAKPOINT,
+  BOX_SHADOW_1,
+  BOX_SHADOW_2
+} from '../util/constants'
 
-/*const getRenderer = ({ src, alt }) => {
-  const typeToRegexp = {
-    video: [
-      /.gifv/,
-      /.mp4/
-    ],
-    img: [
-      /.gif(?!v)/,
-      /.jpg/,
-      /.png/
-    ]
+const Content = styled.div`
+  max-width: ${RESPONSIVE_BREAKPOINT};
+  box-sizing: border-box;
+  margin: 24px auto;
+  padding: 16px;
+  border-radius: 4px;
+  overflow: hidden;
+
+  background: #fff;
+  box-shadow: ${BOX_SHADOW_2};
+
+  div:nth-child(2) {
+    margin-top: 8px;  
+    margin-bottom: 8px;  
   }
-  const typeToRenderer = {
-    video: () => {
-      const videoSrc = [
-        src.split('gifv').join('webm'),
-        src.split('gifv').join('mp4')
-      ]
-      return (
-        <video
-          autoPlay
-          preload
-          loop
-          controls
-        >
-          {videoSrc.map(x => <source src={x} />)}
-        </video>
-      )
-    },
-    img: () => (
-      <img src={src} alt={alt} />
-    ),
-    default: () => (
-      <div>
-        <a
-          target='_blank'
-          rel='noopener noreferrer'
-          href={src}
-        >
-          {src}
-        </a>
-      </div>
-    )
+`
+
+const SelfText = styled.div`
+  font-size: 14px;
+`
+
+const RichMedia = styled.div`
+  margin-left: -16px;
+  margin-right: -16px;
+  margin-bottom: -16px;
+
+  iframe {
+    display: block;
   }
+`
 
-  let mediaType
-  Object.keys(typeToRegexp).forEach(key => {
-    if (mediaType) {
-      return
-    }
-
-    const regs = typeToRegexp[key]
-
-    regs.forEach(reg => {
-      if (reg.test(src)) {
-        mediaType = key
-      }
-    })
-  })
-
-  mediaType = mediaType || 'default'
-  return typeToRenderer[mediaType]()
-} */
+const Comments = Content.extend`
+  box-shadow: ${BOX_SHADOW_1};
+`
 
 class Post extends Component {
   componentDidMount () {
@@ -80,35 +59,39 @@ class Post extends Component {
 
   render () {
     return (
-      <div className='post'>
+      <div>
         <AppBarContainer r={this.props.r} />
 
-        <div className='post__content'>
-          <div className='thing-meta'>
-            <div className='thing-meta__score'>{this.props.score}</div>
+        <Content>
+          <ThreadInformation>
+            <div>
+              <ThreadScore>{this.props.score}</ThreadScore>
+            </div>
             <div>
               Posted by <Link to={`/u/${this.props.author}`}>{this.props.author}</Link>
             </div>
-          </div>
+          </ThreadInformation>
 
           <ThreadHeader {...this.props} />
 
           {/* Only for selfs posts with texts */}
           {this.props.selftext && (
-            <div className='post__body'>
+            <SelfText>
               <Markdown source={this.props.selftext} />
-            </div>
+            </SelfText>
           )}
 
           {/* Only for Rich:Video content */}
           {this.props.post_hint === 'rich:video' && (
-            <div className='post__media'>
+            <RichMedia>
               <FluidIframe {...this.props.media_embed} />
-            </div>
+            </RichMedia>
           )}
-        </div>
+        </Content>
 
-        <CommentsContainer id={this.props.id} />
+        <Comments>
+          <CommentsContainer id={this.props.id} />
+        </Comments>
       </div>
     )
   }
