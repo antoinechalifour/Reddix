c9faf8f2c8492ba18711ef78dbe2aa9d30c2299c
antoinechalifour <antoine.chalifour@gmail.com>
2017-06-05 10:53:39 +0200

    Refactor ThreadHeader to use styled-components

--- src/components/ThreadHeader.js
+++ src/components/ThreadHeader.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import ThumbnailContainer from '../containers/ThumbnailContainer'
 
+const Outer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 18px;
+
+  a:last-child {
+    flex: 1;
+    text-decoration: none;
+    color: inherit;
+  }
+`
+
 const ThreadHeader = props => {
   return (
-    <div className='thread-header'>
+    <Outer>
       <ThumbnailContainer {...props} />
 
       <Link
@@ -12,7 +26,7 @@ const ThreadHeader = props => {
       >
         {props.title}
       </Link>
-    </div>
+    </Outer>
   )
 }
 
